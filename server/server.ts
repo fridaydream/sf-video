b547e19077808c35fa4436348c8a03317a38bf23
chenpan1@sfmail.sf-express.com <chenpan1@sfmail.sf-express.com>
2020-11-09 00:03:28 +0800

    init

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,19 @@ const isDev = false
 
 const app = new Koa();
 
-app.use(bodyparser())
+// app.use(bodyparser())
+
+const router = new Router<DefaultState, Context>({
+  prefix: '/api'
+});
+
+router.post('/user/login', handleLogin)
+router.get('/user/info', handleUserInfo)
+router.get('/video/info', handleVideo)
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.keys = ['some secret hurr'];
 
@@ -53,42 +65,41 @@ const errorCatch = async (ctx: Koa.Context, next: () => void) => {
 
 app.use(errorCatch);
 
-if (!isDev) {
-  // 开发的时候用import需要放在最外面(这个文件可能没有)
-  const serverEntry = require('../dist/server-entry')
-  let template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8')
-  app.use(async (ctx, next) => {
-    ctx.template = template;
-    ctx.serverBundle = serverEntry;
-    await next()
-  });
-  app.use(serverRender);
-  // app.use(async (ctx, next) => {
-  //   console.log('ctx.path', ctx.path)
-  //   if (ctx.path.indexOf('/public/') === 0) {
-  //     ctx.path = ctx.path.slice('/public/'.length);
-  //     await send(ctx, ctx.path, {
-  //       index: 'index.html',
-  //       root: path.join(__dirname, '../dist')
-  //     })
-  //   }
-  //   await next()
-  // })
-} else {
-  devStatic(app)
-}
-
-const router = new Router<DefaultState, Context>({
-  prefix: '/api'
+// 开发的时候用import需要放在最外面(这个文件可能没有)
+const serverEntry = require('../dist/server-entry')
+let template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8')
+app.use(async (ctx, next) => {
+  ctx.template = template;
+  ctx.serverBundle = serverEntry;
+  await next()
 });
+app.use(serverRender);
 
-router.post('/user/login', handleLogin)
-router.get('/user/info', handleUserInfo)
-router.get('/video/info', handleVideo)
+// if (!isDev) {
+//   // 开发的时候用import需要放在最外面(这个文件可能没有)
+//   const serverEntry = require('../dist/server-entry')
+//   let template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8')
+//   app.use(async (ctx, next) => {
+//     ctx.template = template;
+//     ctx.serverBundle = serverEntry;
+//     await next()
+//   });
+//   app.use(serverRender);
+//   // app.use(async (ctx, next) => {
+//   //   console.log('ctx.path', ctx.path)
+//   //   if (ctx.path.indexOf('/public/') === 0) {
+//   //     ctx.path = ctx.path.slice('/public/'.length);
+//   //     await send(ctx, ctx.path, {
+//   //       index: 'index.html',
+//   //       root: path.join(__dirname, '../dist')
+//   //     })
+//   //   }
+//   //   await next()
+//   // })
+// } else {
+//   devStatic(app)
+// }
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
 
 // app.listen(3333, () => {
 //   console.log('server is listening in 3333')
